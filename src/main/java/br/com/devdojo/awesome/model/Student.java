1ee72d337326361f6e50c5e467a8d7d1f0d2ecae
Monique Martins <moniquenegreiros131@gmail.com>
2023-01-19 00:25:18 -0300

    - Criacao de endpoint list; - Adicionada lib do lombok; - Adicionadas configuracoes iniciais da aplicacao.

--- src/main/java/br/com/devdojo/awesome/model/Student.java
+++ src/main/java/br/com/devdojo/awesome/model/Student.java
@@ -1,13 +1,15 @@
 package br.com.devdojo.awesome.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private String name;
-
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public Student(){
-    }
 
 }
